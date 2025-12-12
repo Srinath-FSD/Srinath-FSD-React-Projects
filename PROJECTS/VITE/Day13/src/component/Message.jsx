3680783d62c84895dcf44d034aadc9296d45ed6c
Srinath-FSD <srinath35fsd@gmail.com>
2025-12-12 09:45:59 +0530

    Day13 Task Complted

---
+++ PROJECTS/VITE/Day13/src/component/Message.jsx
@@ -0,0 +1,14 @@
+const Message = () => {
+  return (
+    <p style={{
+      textAlign: "center",
+      fontSize: "18px",
+      marginTop: "10px",
+      color: "gray"
+    }}>
+      This is a simple React component page with style.
+    </p>
+  );
+};
+
+export default Message;
